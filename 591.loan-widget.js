f9cbe443fad293cc5298251134a66e82d0215312
Mohit Sengar <andreievandrii71@gmail.com>
2024-07-13 12:11:31 +0300

    first commit

---
+++ 591.loan-widget.js
@@ -0,0 +1,1 @@
+"use strict";(this.webpackChunkLoanWidgetLibrary=this.webpackChunkLoanWidgetLibrary||[]).push([[591],{591:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
